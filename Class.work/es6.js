fe1755164fc7e4a9e537b6adfa31965885f648a3
Rudransh-Gupta0304 <rudransh.25b01010390@abes.ac.in>
2026-09-12 20:38:49 +0530

    Backend FSD

---
+++ Class.work/es6.js
@@ -0,0 +1,22 @@
+// javascript
+// function : block of codre 
+//  syntax
+//  function fname(){
+// block of code
+// }
+//  function calling 
+
+
+// function sayHello(a , b){
+//     console.log("hello world");
+//     return a + b ;
+// }
+
+// console.log(sayHello(5 , 9));
+
+
+const  sayHello= (()  => {
+    console.log("arrow function")
+})
+
+sayHello(); 
