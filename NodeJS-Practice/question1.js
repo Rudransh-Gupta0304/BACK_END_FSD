956dbbcafac093164ea46718fbc1b12d50284ad5
Rudransh-Gupta0304 <rudransh.25b01010390@abes.ac.in>
2026-09-15 09:09:30 +0530

    Updated

--- NodeJS-Practice/question1.js
+++
@@ -1,9 +0,0 @@
-const myPromise = new Promise((resolve) => {
-    setTimeout(() => {
-        resolve("Data fetched successfully");
-    }, 2000);
-});
-
-myPromise.then((result) => {
-    console.log(result);
-})
